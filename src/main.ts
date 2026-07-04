3a959f56fabad1b78ae542ef13d120e01e279227
Mpras03 <54835304+Mpras03@users.noreply.github.com>
2026-07-04 10:58:45 +0700

    setup swagger API, dan install package yang dibutuhkan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Bulmar API')
+    .setDescription('API Documentation Bulmar CMS')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
