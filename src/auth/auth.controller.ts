72eddd196c0232b414476ca31af0271e46a76487
Mpras03 <54835304+Mpras03@users.noreply.github.com>
2026-07-04 19:55:02 +0700

    update : add enpoint login + generate access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Authentication')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  //===================================== ENDPOINT LOGIN ====================================
+  @Post('login')
+  @ApiOperation({
+    summary: 'Login',
+  })
+  login(
+    @Body()
+    loginDto: LoginDto,
+  ) {
+    return this.authService.login(loginDto);
+  }
+  //=========================================================================================
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    //============================== VALIDATE USER ===================================
+    async validateUser(loginDto: LoginDto) {
+        const user = await this.usersService.findByUsername(loginDto.username);
+        if (!user) {
+            throw new UnauthorizedException(
+            'Username atau password salah',
+            );
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            loginDto.password,
+            user.passwordHash,
+        );
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException(
+            'Username atau password salah',
+            );
+        }
+
+        return user;
+    }
+    //=======================================================================
+
+    //=========================== LOGIN =====================================
+    async login(loginDto: LoginDto) {
+        const user = await this.validateUser(loginDto);
+        const payload = {
+            sub: user.id,
+            username: user.username,
+            name: user.name,
+        };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+            user: {
+            id: user.id,
+            username: user.username,
+            name: user.name,
+            },
+        };
+    }
+}
